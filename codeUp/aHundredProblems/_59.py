612c001340b2a860a20f68bd4c1e7bc45f440e34
kimjunyun7 <50650898+kimjunyun7@users.noreply.github.com>
2023-01-28 12:46:45 +0000

    solve 61~69

---
+++ codeUp/aHundredProblems/_59.py
@@ -0,0 +1,7 @@
+'''
+    CodeUp
+    Pyhon 기초 100제 - 59번
+'''
+
+a = ~int(input())
+print(a)
---
+++ codeUp/aHundredProblems/_60.py
@@ -0,0 +1,9 @@
+'''
+    CodeUp
+    Pyhon 기초 100제 - 60번
+'''
+
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a & b)
